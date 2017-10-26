1d93661bc1941590f27e994a712aa46a0541052a
JoshuaBacurioMercado <32262804+JoshuaBacurioMercado@users.noreply.github.com>
2017-10-26 12:11:44 +0200

    asdsadads

--- Yatzy/src/gui/MainApp.java
+++ Yatzy/src/gui/MainApp.java
@@ -27,7 +27,7 @@ public class MainApp extends Application {
 		stage.setResizable(false);
 		stage.show();
 	}
-	// dlasknsa
+	// saklnddask
 
 	// -------------------------------------------------------------------------
 
